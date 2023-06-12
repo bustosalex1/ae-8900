3a8b4137bf992efcffdc1681498d4a9799952f65
Alex Bustos <bustosalex1@gmail.com>
2023-06-12 05:17:21 -0400

    also I guess DND would be cool

--- frontend/src/lib/components/dashboard/Websocket.svelte
+++ frontend/src/lib/components/dashboard/Websocket.svelte
@@ -19,6 +19,22 @@
 	$: if (receivedMessages.length > 5) {
 		receivedMessages.shift();
 	}
+
+	import { flip } from 'svelte/animate';
+	import { dndzone } from 'svelte-dnd-action';
+	let items = [
+		{ id: 1, name: 'item1' },
+		{ id: 2, name: 'item2' },
+		{ id: 3, name: 'item3' },
+		{ id: 4, name: 'item4' }
+	];
+	const flipDurationMs = 300;
+	function handleDndConsider(e) {
+		items = e.detail.items;
+	}
+	function handleDndFinalize(e) {
+		items = e.detail.items;
+	}
 </script>
 
 <h1 class="text-xl">WebSocket Chat</h1>
@@ -32,3 +48,29 @@
 		<li transition:slide={{ axis: 'y', duration: 300 }}>{new Date(message.timestamp * 1000)}</li>
 	{/each}
 </ul>
+<section
+	use:dndzone={{ items, flipDurationMs }}
+	on:consider={handleDndConsider}
+	on:finalize={handleDndFinalize}
+>
+	{#each items as item (item.id)}
+		<div animate:flip={{ duration: flipDurationMs }}>{item.name}</div>
+	{/each}
+</section>
+
+<style>
+	section {
+		width: 50%;
+		padding: 0.3em;
+		border: 1px solid black;
+		/* this will allow the dragged element to scroll the list */
+		overflow: scroll;
+		height: 200px;
+	}
+	div {
+		width: 50%;
+		padding: 0.2em;
+		border: 1px solid blue;
+		margin: 0.15em 0;
+	}
+</style>
